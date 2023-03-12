abc095659c5c6862b2261d7066fc635915f01205
tim <tl853853.fh-muenster.de>
2023-03-12 16:46:17 +0100

    added basic layout and camarapreview

--- app/src/main/java/com/example/digidoc/MainActivity.kt
+++ app/src/main/java/com/example/digidoc/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.digidoc
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material.icons.rounded.PhotoCamera
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.example.digidoc.ui.theme.DigidocTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             DigidocTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,11 +40,92 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Column() {
 
+                        CameraPreview()
+                        PhotoToolbar()
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun PhotoToolbar() {
+
+    Row(
+        Modifier
+            .background(Color(154, 20, 20))
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                Icons.Rounded.Edit,
+                contentDescription = "Edit",
+                Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f)
+                    .padding(10.dp)
+            )
+
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                Icons.Rounded.PhotoCamera,
+                contentDescription = "take picture",
+                Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f)
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                Icons.Rounded.Add,
+                contentDescription = "add",
+                Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f)
+                    .padding(10.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun CameraPreview() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+
+    AndroidView(
+        factory = { ctx ->
+            val previewView = PreviewView(ctx)
+            val executor = ContextCompat.getMainExecutor(ctx)
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = androidx.camera.core.Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    preview
+                )
+            }, executor)
+            previewView
+        },
+        modifier = Modifier.fillMaxHeight(0.9f),
+    )
 }
 
 
